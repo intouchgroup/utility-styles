337e3f8b08a9f54ea784e3359e45e356d5a14cd8
Cody Persinger <cody.persinger@intouchsol.com>
2020-04-23 14:05:44 -0500

    Update dependencies, update _media.scss and _text.scss generators, bump version to 0.0.7

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ const { createWebpackConfig } = require('@intouchgroup/es2020-boilerplate');
 module.exports = (env, argv) => createWebpackConfig({
     argv,
     entries: {
-        styles: './styles.scss',
+        styles: './index.scss',
     },
 });
